af93500ac9dfa95f3300c89bd583447ff4581815
Peder Goodman <peder.goodman@gmail.com>
2023-05-18 10:21:01 -0500

    answer question 1b.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -20,6 +20,12 @@ for (let i=0; i<6; i++){
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
+console.log('count from 3 to 5');
+for (let i=0; i<6; i++){
+  if (i>2 && i<6){
+  console.log(i);
+  }
+}
 
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
