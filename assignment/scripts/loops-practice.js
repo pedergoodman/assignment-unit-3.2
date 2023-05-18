c962269fb078d152f1391aa911bc742f05335972
Peder Goodman <peder.goodman@gmail.com>
2023-05-18 10:34:48 -0500

    Added for..of statment to 2a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -52,18 +52,29 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
+
+console.log("\tfor..in");
+// Used a for...in statement initially
 for (i in stars) {
   console.log(stars[i]);
+}
+console.log("\tfor..of");
+// Here's the for of statement
+for (i of stars) {
+  console.log(i);
 }
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
+
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
